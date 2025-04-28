4cc0c60aef9f14dfcbb53b75bebf51b6ee3aa78b
Layle <ioncodes@users.noreply.github.com>
2025-04-28 18:35:08 +0200

    fixed more checksum checks and hook for fake ProgressiveDecompress function that checks CD stuff

--- dllmain.cpp
+++ dllmain.cpp
@@ -26,8 +26,11 @@
 #pragma comment(linker, "/export:DllCanUnloadNow=d3drm_ori.DllCanUnloadNow,@22")
 #pragma comment(linker, "/export:DllGetClassObject=d3drm_ori.DllGetClassObject,@23")
 
+using ProgressiveDecompress_t = DWORD(__stdcall*)(CHAR driveletter, __int16 value);
+
 decltype(GetVolumeInformationA)* g_OriginalGetVolumeInformationA = nullptr;
 decltype(GetDriveTypeA)* g_OriginalGetDriveTypeA = nullptr;
+ProgressiveDecompress_t* g_OriginalProgressiveDecompress = nullptr;
 
 BOOL WINAPI GetVolumeInformationA_hk(
     LPCSTR lpRootPathName,
@@ -87,6 +90,31 @@ UINT WINAPI GetDriveTypeA_hk(LPCSTR lpRootPathName)
 	return result;
 }
 
+DWORD __stdcall ProgressiveDecompress_hk(CHAR driveletter, __int16 value)
+{
+	// value == 2 => 004386AA | 817D F8 1B324100 | cmp dword ptr ss:[ebp-8],legorr.41321B
+	// value == 3 => .text:00479893                cmp     [ebp+var_4], 43002Fh
+	
+	DWORD result = 0;
+
+	switch (value)
+	{
+	case 2:
+		result = 0x0041321B;
+		break;
+	case 3:
+		result = 0x0043002F;
+		break;
+	default:
+		result = (DWORD)"LAYL"; // Shoudl never happen, at least not for LRR
+		break;
+	}
+
+	std::cout << "ProgressiveDecompress(\'" << driveletter << "\', " << value << ") -> " << std::hex << result << std::endl;
+	
+	return result;
+}
+
 VOID NopChecksumCheck(BYTE* pAddress, size_t size)
 {
 	/*
@@ -100,6 +128,8 @@ VOID NopChecksumCheck(BYTE* pAddress, size_t size)
 		[ESP] -> points to next function call, will break if checksum isn't 0
 
 		pattern: 2B 42 04 29 04 24 33 C0 64 89 00
+		new finding: the checksums are also performed against DECO_24.dll
+		universal pattern: 03 06 46 49 75 FA
 	*/
 
 	DWORD oldProtect;
@@ -109,34 +139,34 @@ VOID NopChecksumCheck(BYTE* pAddress, size_t size)
 	VirtualProtect(pAddress, size, oldProtect, &oldProtect);
 }
 
-LONG CALLBACK VectoredHandler(EXCEPTION_POINTERS* ExceptionInfo)
-{
-	static int hitCounter = 0;
-
-	// This exception address occurs anyways, let's just reuse it
-	if (ExceptionInfo->ExceptionRecord->ExceptionAddress == (PVOID)0x0047983A && hitCounter > 5 && hitCounter != 0xFF)
-	{
-		// Write "LAYL" to the flag, anything non-zero works
-		BYTE* pLol = reinterpret_cast<BYTE*>(0x0076D160);
-		pLol[0] = 'L';
-		pLol[1] = 'A';
-		pLol[2] = 'Y';
-		pLol[3] = 'L';
-
-		hitCounter = 0xFF; // Oracle to not trigger this again
-
-		std::cout << "Patched upgrade base flag" << std::endl;
-	}
-	else if (ExceptionInfo->ExceptionRecord->ExceptionAddress == (PVOID)0x0047983A && hitCounter != 0xFF)
-	{
-		hitCounter++;
-
-		std::cout << "Hit exception " << std::hex << ExceptionInfo->ExceptionRecord->ExceptionAddress << " [" << hitCounter << "]. Waiting..." << std::endl;
-	}
-
-	// There should be another handler available
-	return EXCEPTION_CONTINUE_SEARCH;
-}
+//// We don't need this since we now hook DECO_24.DLL
+//LONG CALLBACK VectoredHandler(EXCEPTION_POINTERS* ExceptionInfo)
+//{
+//	static int hitCounter = 0;
+//
+//	// This exception address occurs anyways, let's just reuse it
+//	if (ExceptionInfo->ExceptionRecord->ExceptionAddress == (PVOID)0x0047983A && hitCounter > 5 && hitCounter != 0xFF)
+//	{
+//		// Write "LAYL" to the flag, anything non-zero works
+//		BYTE* pLol = reinterpret_cast<BYTE*>(0x0076D160);
+//		pLol[0] = 'L';
+//		pLol[1] = 'A';
+//		pLol[2] = 'Y';
+//		pLol[3] = 'L';
+//
+//		hitCounter = 0xFF; // Oracle to not trigger this again
+//
+//		std::cout << "Patched upgrade base flag" << std::endl;
+//	}
+//	else if (ExceptionInfo->ExceptionRecord->ExceptionAddress == (PVOID)0x0047983A && hitCounter != 0xFF)
+//	{
+//		std::cout << "Hit exception " << std::hex << ExceptionInfo->ExceptionRecord->ExceptionAddress << " [" << hitCounter << "]. Waiting..." << std::endl;
+//		hitCounter++;
+//	}
+//
+//	// There should be another handler available
+//	return EXCEPTION_CONTINUE_SEARCH;
+//}
 
 VOID ApplyPatches()
 {
@@ -167,6 +197,14 @@ VOID ApplyPatches()
 	) != MH_OK)
 		std::cout << "Failed to create hook for GetDriveTypeA" << std::endl;
 
+	if (MH_CreateHookApi(
+		L"DECO_24.dll",
+		"ProgressiveDecompress_24",
+		ProgressiveDecompress_hk,
+		reinterpret_cast<LPVOID*>(&g_OriginalProgressiveDecompress)
+	) != MH_OK)
+		std::cout << "Failed to create hook for ProgressiveDecompress" << std::endl;
+
 	MH_EnableHook(MH_ALL_HOOKS);
 
 	// *** CHECKSUM PATCHES ***
@@ -179,8 +217,14 @@ VOID ApplyPatches()
 	
 	NopChecksumCheck(reinterpret_cast<BYTE*>(0x0043872B), 3); // Right before upgrading the base
 	std::cout << "Patched checksum check (Base Upgrade)" << std::endl;
+	
+	NopChecksumCheck(reinterpret_cast<BYTE*>(0x004386F1), 3); // DECO_24.dll Checksum 1
+	std::cout << "Patched checksum check (DECO_24.DLL Checksum 1)" << std::endl;
+	
+	NopChecksumCheck(reinterpret_cast<BYTE*>(0x004798DC), 3); // DECO_24.dll Checksum 2
+	std::cout << "Patched checksum check (DECO_24.DLL Checksum 2)" << std::endl;
 
-	// *** PATCH UPGRADE BASE ***
+	// *** PATCH UPGRADE BASE (OLD) ***
 
 	//// Invert the condition for upgrading the base, IDK what the condition is but it works
 	//BYTE* pAddress = reinterpret_cast<BYTE*>(0x004386A8);
@@ -194,7 +238,9 @@ VOID ApplyPatches()
 	//}
 
     // Add VEH to patch the upgrade base conditional after the init code at 0x004781C3
-    AddVectoredExceptionHandler(1, VectoredHandler);
+    //AddVectoredExceptionHandler(1, VectoredHandler);
+
+
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
